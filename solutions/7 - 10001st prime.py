bec0f58d2d0ed03262c3e861efa4ca95fd9be690
s-nikravesh995 <s.nikravesh995@gmail.com>
2020-02-05 21:24:09 +0330

    Added solution of problem #7 (second approach).

--- solutions/7 - 10001st prime.py
+++ solutions/7 - 10001st prime.py
@@ -46,3 +46,44 @@ for i in prime_gen():
         break
 
 print(result)
+
+###############################################################################
+
+# Approach 2:
+# Another alternative is to find a upper bound that can guarantee there are
+# at least 10001 primes up to this limit.
+# We can use the inverse of prime counting function to find this limit.
+
+
+def pi_inverse(n):
+    """Returns an upper bound for prime counting inverse function (pi function).
+    """
+    upperbnd = (n + 1) * (log(n + 1) + log(log(n + 1)))
+    return ceil(upperbnd)
+
+
+def primes(n) -> list:
+    """Returns primes less than n using Eratosthenes sieve algorithm."""
+    if n <= 1:
+        raise ValueError("invalid number")
+
+    isprime = [True] * (n - 1)
+    primes_list = []
+    i = 2
+    while i ** 2 <= n:
+        if isprime[i - 2]:
+            j = i ** 2
+            while j <= n:
+                isprime[j - 2] = False
+                j += i
+        i += 1
+    for num in range(2, n - 1):
+        if isprime[num - 2]:
+            primes_list.append(num)
+    return primes_list
+
+
+limit = pi_inverse(10001)
+primes_list = primes(limit)
+result = primes_list[10000]  # Note that 10001 element has the index of 10000.
+print(result)
